860896265be4187da372c51ab8acf091e3a0e7ea
Simon Knott <simoknott@gmail.com>
2017-12-10 18:01:46 +0100

    Implement PrioQueue

--- Musikplayer/src/PriorityQueue.java
+++ Musikplayer/src/PriorityQueue.java
@@ -1,114 +1,137 @@
-import com.sun.istack.internal.NotNull;
-import com.sun.istack.internal.Nullable;
-
-/**
- * Created by simon.knott on 17.11.2017.
- */
 public class PriorityQueue<ContentType> {
-    private class PriorityNode<ContentType> {
-        private final ContentType content;
-        private PriorityNode<ContentType> next;
-        private final int priority;
+  // private Klasse fuer ein Element der Schlange
+  private class PriorityNode {
+    private ContentType content;
+    private PriorityNode nextNode;
+    private final int priority;
 
-        /**
-         *
-         * @param content
-         */
-        public PriorityNode(ContentType content, int priority) {
-            this.content = content;
-            this.priority = priority >= 0 ? priority : 0;
-        }
-
-        public ContentType getContent() {
-            return content;
-        }
-
-        public int getPriority() {
-            return priority;
-        }
-
-        @Nullable
-        public PriorityNode<ContentType> getNext() {
-            return next;
-        }
-
-        public void setNext(PriorityNode<ContentType> next) {
-            this.next = next;
-        }
-    }
-    /*
-     * Attributes
-     */
-    private PriorityNode<ContentType> head;
-    private PriorityNode<ContentType> tail;
-
-    /*
-     * # Interface
-     */
-    /**
-     * ## Standard
-     */
-
-    public void enqueue(@NotNull ContentType value, int priority) {
-      if (value == null) return;
-
-      PriorityNode<ContentType> node = new PriorityNode<>(value, priority);
-
-      // Leere Schlange
-      if (isEmpty()) {
-        this.head = node;
-        this.tail = node;
-      } else if (node.priority == 0) {
-        tail.setNext(node);
-        tail = node;
-      } else {
-        PriorityNode<ContentType> temp = head;
-        while (temp.getNext() != null && temp.getPriority() >= node.getPriority()) temp = temp.getNext();
-
-        temp.setNext(node);
-        node.setNext(temp.getNext());
-      }
+    public PriorityNode(ContentType content, int priority) {
+      this.content = content;
+      this.priority = priority;
     }
 
-    /**
-     * Pop the first value off the stack
-     * @return first value
-     */
-    @Nullable
-    public void dequeue() {
-        if (isEmpty()) return;
-
-        head = head.getNext();
+    public void setNext(PriorityNode pNext) {
+      nextNode = pNext;
     }
 
-    /**
-     * Get Value of front PriorityNode without removing it
-     * @return null if empty
-     */
-    @Nullable
-    public ContentType front() {
-        if (isEmpty()) return null;
-
-        return head.getContent();
+    public PriorityNode getNext() {
+      return nextNode;
     }
 
-    /**
-     * Checks if the stack is empty
-     * @return true if empty
-     */
-    public boolean isEmpty() {
-        return head == null;
+    public ContentType getContent() {
+      return content;
     }
 
-    public static void main(String... args) {
-        PriorityQueue<String> queue = new PriorityQueue<>();
-
-        queue.enqueue("Prio 0!", 100);
-        queue.enqueue("Prio 1!", 10);
-
-        while(!queue.isEmpty()) {
-            System.out.println(queue.front());
-            queue.dequeue();
-        }
+    public int getPriority() {
+      return priority;
     }
+  } // Ende private Klasse fuer ein Element
+
+  // Attribute der Klasse Queue
+  private PriorityNode head;
+  private PriorityNode tail;
+
+  // Methoden der Klasse Queue
+
+  /**
+   * Eine leere Schlange wird erzeugt. Objekte, die in dieser Schlange
+   * verwaltet werden, muessen vom Typ ContentType sein.
+   */
+  public PriorityQueue() {
+    head = null;
+    tail = null;
+  }
+
+  /**
+   * Die Anfrage liefert den Wert true, wenn die Schlange keine Objekte
+   * enthaelt, sonst liefert sie den Wert false.
+   *
+   * @return true, falls die Schlange leer ist, sonst false
+   */
+  public boolean isEmpty() {
+    return head == null;
+  }
+
+  /**
+   * Das Objekt pContent wird an die Schlange angehaengt. Falls
+   * pContent gleich null ist, bleibt die Schlange unveraendert.
+   *
+   * @param pContent das anzuhaengende Objekt
+   */
+  public void enqueue(ContentType pContent, int priority) {
+    if (pContent == null) return;
+
+    PriorityNode newNode = new PriorityNode(pContent, priority);
+
+    // Case: Queue ist leer
+    if (isEmpty()) {
+      head = newNode;
+      tail = newNode;
+    }
+    // Case: newNode muss an den Schluss
+    else if (priority == 0 || newNode.getPriority() <= tail.getPriority()){
+      tail.setNext(newNode);
+      tail = newNode;
+    }
+    // Case: newNode muss an den Beginn
+    else if (newNode.getPriority() > head.getPriority()) {
+      newNode.setNext(head);
+      head = newNode;
+    }
+    // Default Case
+    else {
+      PriorityNode node = head;
+
+      // Node finden, deren Nachfolger eine kleinere Prio als `newNode` hat
+      while (node.getNext().getPriority() >= newNode.getPriority()) node = node.getNext();
+
+      // `newNode` zwischen `node` und `node.getNext()` platzieren
+      newNode.setNext(node.getNext());
+      node.setNext(newNode);
+    }
+  }
+
+  /**
+   * Das erste Objekt wird aus der Schlange entfernt. Falls die Schlange
+   * leer ist, wird sie nicht veraendert.
+   */
+  public void dequeue() {
+    if (!this.isEmpty()) {
+      head = head.getNext();
+    }
+  }
+
+  /**
+   * Die Anfrage liefert das erste Objekt der Schlange. Die Schlange
+   * bleibt unveraendert. Falls die Schlange leer ist, wird null zurueckgegeben.
+   *
+   * @return das erste Objekt der Schlange oder null, falls
+   * die Schlange leer ist.
+   */
+  public ContentType front() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      return head.getContent();
+    }
+  }
+
+  public static void main(String... args) {
+    PriorityQueue<String> queue = new PriorityQueue<>();
+
+    for (int i = 10; i < 100; i += 3) queue.enqueue("Prio " + i, i);
+
+    queue.enqueue("Prio 32", 32);
+    queue.enqueue("Prio 32 #2", 32);
+    queue.enqueue("Prio 32 #3", 32);
+    print(queue);
+  }
+
+  private static void print(PriorityQueue queue) {
+    System.out.println("----");
+    while (!queue.isEmpty()) {
+      System.out.println(queue.front());
+      queue.dequeue();
+    }
+  }
 }
